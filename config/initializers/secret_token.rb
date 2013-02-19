763391841f13189880ae6787e8ac3c273bc695a4
Karlis <karlis@karlis-VirtualBox.(none)>
2013-02-19 16:23:45 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kurjers::Application.config.secret_token = 'cba5c834427f502cb8789e6f311ad3e5b1a66d3a4dc724193edb3abcc78e2839b763d2997fb0c52f8bb49723760793d1ee98a2f3f70876a02185a5fa8ddf6886'
